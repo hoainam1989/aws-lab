2d42db2f3bfaa09051977b916b288780e496dd5a
nam.dinh <test@a.com>
2017-12-27 14:18:33 +0700

    revert to demo opsworks

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ const app = express()
 var config = require('config')
 var mysql      = require('mysql')
 
-console.log('config',config)
+// console.log('config',config)
 
-var connection = mysql.createConnection({
-  host     : config.get('DB_ENDPOINT'),
-  user     : config.get('DB_USER'),
-  password : config.get('DB_PASS'),
-  database : config.get('DB_NAME')
-});
+// var connection = mysql.createConnection({
+//   host     : config.get('DB_ENDPOINT'),
+//   user     : config.get('DB_USER'),
+//   password : config.get('DB_PASS'),
+//   database : config.get('DB_NAME')
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
-})
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error('error connecting: ' + err.stack);
+//     return;
+//   }
+//   console.log('connected as id ' + connection.threadId);
+// })
 
 app.get('/hello', function (req, res) {
   res.status(200).send({'blog':'hd7exploit'}); 
