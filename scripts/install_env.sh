0d87c1306638062139e53cf4b486b5c201072e26
nam.dinh <test@a.com>
2018-01-15 22:22:41 +0700

    code deploy

---
+++ scripts/install_env.sh
@@ -0,0 +1,5 @@
+curl -sL https://rpm.nodesource.com/setup_6.x | bash -
+yum install -y nodejs
+yum install gcc-c++ make -y
+npm install -g forever
+yum install git -y
---
+++ scripts/start_app.sh
@@ -0,0 +1,3 @@
+cd /home/ec2-user/nodeapp
+npm install
+forever start index.js
